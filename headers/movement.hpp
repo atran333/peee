6c54f183c15686d1ecc5645f2b2dd554a29745a0
ashwinjagadish1 <ashwin.jagadish1@gmail.com>
2021-11-15 22:39:12 -0800

    added movement.cpp and movement.hpp

---
+++ headers/movement.hpp
@@ -0,0 +1,16 @@
+#ifndef MOVEMENT_H
+#define MVOEMENT_H
+
+#include <iostream>
+#include <vector>
+#include <SMFL/Graphics.hpp>
+#include <SFML/System.hpp>
+
+class movement
+{
+  public:
+     ;void getPosition();
+     ;void setPosition();
+     int update();
+};
+#endif
