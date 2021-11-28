1cf2198d1e3a76145727cd6c6c8c8de75bda5448
Andy Tran <andytran@Steves-MacBook-Air.local>
2021-11-28 01:35:10 -0800

    implemented State header

---
+++ header/State.h
@@ -0,0 +1,40 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include <fstream>
+#include <sstream>
+#include <stack>
+#include <map>
+#include <vector>
+
+#include "Game.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <vector> 
+
+class State
+{
+    private:
+        sf::RenderWindow* window;
+        std::vector<sf::Texture*> textures;
+        bool quit;
+    public:
+        //saves sending it in all the time
+        State(sf::RenderWindow* window);
+        virtual ~State();
+        const bool& getQuit() const;
+        virtual void endState() = 0;
+        virtual void checkForQuit();
+        virtual void updateKeyBinds(const float& deltaTime);
+        virtual void update(const float& deltaTime) = 0;
+        // if nothing is given in the parameters, we'll run into the window, if u want to send a specific target, send in the parameter
+        virtual void render(sf::RenderTarget* target = nullptr) = 0;
+};
+#endif //
