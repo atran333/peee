6de79e1183f3eaf09c81c2e5633ddfc44f5fd357
Andy Tran <andytran@ucr-guest-01-10-14-2-132.wnet.ucr.edu>
2021-11-29 00:56:28 -0800

    added double stack pointer

--- header/State.hpp
+++ header/State.hpp
@@ -21,18 +21,20 @@
 
 class State
 {
-    private:
+    protected:
         sf::RenderWindow* window;
+        std::stack<State*>* states;
         std::vector<sf::Texture*> textures;
         bool quit;
     public:
         //saves sending it in all the time
-        State(sf::RenderWindow* window);
+        
+        State(sf::RenderWindow* window, std::stack<State*>* states);
         virtual ~State();
         const bool& getQuit() const;
         virtual void endState() = 0;
         virtual void checkForQuit();
-        virtual void updateKeyBinds(const float& deltaTime);
+        virtual void updateInput(const float& deltaTime);
         virtual void update(const float& deltaTime) = 0;
         // if nothing is given in the parameters, we'll run into the window, if u want to send a specific target, send in the parameter
         virtual void render(sf::RenderTarget* target = nullptr) = 0;
